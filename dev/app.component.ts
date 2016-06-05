66fa253a616c189284a9e3716d9c927793c74084
hogvarce <hogvarce@gmail.com>
2016-06-05 23:18:27 +0300

    урок с формами

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,15 +1,36 @@
 import {Component} from 'angular2/core';
-import {PuzzleComponent} from './puzzle/puzzle.component';
+import {InputComponent} from './bindings/input.component';
+import {ConfirmComponent} from './bindings/confirm.component';
 
 @Component({
     selector: 'app',
     template: `
-        <puzzle></puzzle>
+        <div class="container">
+            <my-input (submitted)="onSubmit($event)" [myself]="confirmedMyself"></my-input>
+        </div>
+        <div class="container">
+            <my-confirm (confirmed)="onConfirm($event)" [myself]="myself"></my-confirm>
+        </div>
     `,
     directives: [
-        PuzzleComponent
+        InputComponent,
+        ConfirmComponent
     ]
+
 })
 export class AppComponent {
-
+    myself = {
+        name: '',
+        age: ''
+    };
+    confirmedMyself = {
+        name: '',
+        age: ''
+    }
+    onSubmit(myself: {name: string, age: string}){
+        this.myself = {name: myself.name, age: myself.age};
+    }
+    onConfirm(myself: {name: string, age: string}){
+        this.confirmedMyself = {name: myself.name, age: myself.age};
+    }
 }
